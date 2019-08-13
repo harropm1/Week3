077e70d62205aa4ac3d83edf1372f65c11ea69ac
Michelle Harrop <harrop.michelle@gmail.com>
2019-08-13 09:12:09 -0400

    I messed up and worked on master. oops

--- Mod3/Events/scripts/events.js
+++ Mod3/Events/scripts/events.js
@@ -1,2 +1,45 @@
 "use strict";
 
+window.onload = function()
+{
+    const allTextInputFields = document.querySelectorAll("input[type='text']");
+    //this is to make the input fields tan
+    for (let i = 0; i < allTextInputFields.length; i++)
+    {
+        allTextInputFields[i].onfocus = function()
+        {
+            this.style.backgroundColor = "tan";
+        };
+        allTextInputFields[i].onblur = function()
+        {
+            this.style.backgroundColor = "";
+        };
+    }
+
+    const stateInputField = document.getElementById("stateInput");
+    //this is to make the letters in the state capitalize
+    stateInputField.oninput = function()
+    {
+        stateInputField.value = stateInputField.value.toUpperCase();
+    };
+
+    const billingStateInputField = document.querySelector("#billingStateInput");
+    billingStateInputField.oninput = function()
+    {
+        billingStateInputField.value = billingStateInputField.value.toUpperCase();
+    };
+
+    const sSameAsBDDL = document.querySelector("#sSameAsB");
+    let billingForm = document.querySelector("#billing");
+    sSameAsBDDL.onchange = function()
+    {
+        if (sSameAsBDDL.value == "Same Address")
+        {
+            billingForm.style.display = "none";
+        }
+        else 
+        {
+            billingForm.style.display = "block";
+        }
+    }
+}
